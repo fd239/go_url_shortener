6457babcb074bb0baa581ef3c846cd64c0e5f4b4
Dmitriy Frolov <62940601+fd239@users.noreply.github.com>
2022-06-20 01:36:12 +0300

    Increment 19. build flags added

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,12 +1,23 @@
 package main
 
 import (
+	"fmt"
 	"github.com/fd239/go_url_shortener/internal/app/common"
 	"github.com/fd239/go_url_shortener/internal/app/server"
 	"log"
 )
 
+var (
+	buildVersion = "N/A"
+	buildDate    = "N/A"
+	buildCommit  = "N/A"
+)
+
 func main() {
+	fmt.Printf("Build version: %v\n", buildVersion)
+	fmt.Printf("Build date: %v\n", buildDate)
+	fmt.Printf("Build commit: %v\n", buildCommit)
+
 	err := common.InitConfig()
 
 	if err != nil {
